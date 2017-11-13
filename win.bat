ef36a72619fbbbd4f2854ec5b7253ae077badf96
MEIE\37503294229 <aare.puusepp@smit.ee>
2017-11-13 13:55:14 +0200

    VI. Processes

---
+++ win.bat
@@ -0,0 +1,8 @@
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_152
+SET PATH=%JAVA_HOME%\bin;%PATH%
+
+SET ANT_HOME=C:\usr\local\apache-ant-1.9.6
+SET PATH=%ANT_HOME%\bin;%PATH%
+
+SET MAVEN_HOME=C:\usr\local\apache-maven-3.5.2
+SET PATH=%MAVEN_HOME%\bin;%PATH%
